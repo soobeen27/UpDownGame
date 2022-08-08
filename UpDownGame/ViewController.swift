c4378d52bac3fdb97e8642e237e15d3c55ff24ae
Soobeen Jang <youdew27@icloud.com>
2022-08-08 16:52:57 +0900

    updown game

--- UpDownGame/ViewController.swift
+++ UpDownGame/ViewController.swift
@@ -8,12 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var tappedNumLabel: UILabel!
+    var comNum = Int.random(in: 1...10)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        statusLabel.text = "choose your num"
+        tappedNumLabel.text = ""
     }
 
-
+    
+    @IBAction func numButtonTapped(_ sender: UIButton) {
+        guard let numString = sender.currentTitle else { return }
+        tappedNumLabel.text = numString
+    }
+    
+    @IBAction func resetButtonTapped(_ sender: UIButton) {
+        statusLabel.text = "choose your num"
+        tappedNumLabel.text = ""
+        comNum = Int.random(in: 1...10)
+    }
+    
+    @IBAction func selectButtonTapped(_ sender: UIButton) {
+        guard let numString = tappedNumLabel.text else { return }
+        guard let num = Int(numString) else { return}
+        
+        if comNum > num {
+            statusLabel.text = "Up"
+        } else if comNum < num {
+            statusLabel.text = "Down"
+        } else {
+            statusLabel.text = "Bingo"
+        }
+    }
 }
 
